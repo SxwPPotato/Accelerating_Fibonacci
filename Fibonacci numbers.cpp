a780565314e07d1f680c731d863809f0f41b455f
Vitalya <vitalyabes.vb@gmail.com>
2023-12-12 10:15:56 +0300

    Second commit

--- Fibonacci numbers.cpp
+++ Fibonacci numbers.cpp
@@ -1,15 +1,22 @@
 ﻿#include <iostream>
 
-unsigned long Fibonachi(int num) {
-
-    if (num == 1)
+unsigned long Fibonachi(int* memory_mass,int num) {
+   
+    if (memory_mass[num - 1] != NULL) {
+        return  memory_mass[num - 1];
+    }
+    else if (num == 1){
+        memory_mass[num - 1] = 0;
         return 0;
-
-    else if (num == 2)
+}
+    else if (num == 2) {
+        memory_mass[num - 1] = 1;
         return 1;
-
-    else
-        return Fibonachi(num - 2) + Fibonachi(num - 1);
+    }
+    else {
+        return Fibonachi(memory_mass, num - 2) + Fibonachi(memory_mass, num - 1);
+        memory_mass[num - 1] = Fibonachi(memory_mass, num - 2) + Fibonachi(memory_mass, num - 1);
+    }
 }
 
 
@@ -18,9 +25,10 @@ int main() {
     int num;
     std::cout << "Введите количество чисел: ";
     std::cin >> num;
+    int* memory_mass = new int[num]();
     std::cout << "Числа Фибоначчи: ";
     for (int i = 1; i <= num; ++i) {
-        std::cout << Fibonachi(i) << "\t";
+        std::cout << Fibonachi(memory_mass, i) << "\t";
 
     }
     return 0;
