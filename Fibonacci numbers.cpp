327d36e641a73574f0f879f551cfe084350afa52
Vitalya <vitalyabes.vb@gmail.com>
2023-12-12 09:28:02 +0300

    Initial commit

---
+++ Fibonacci numbers.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+unsigned long Fibonachi(int num) {
+
+    if (num == 1)
+        return 0;
+
+    else if (num == 2)
+        return 1;
+
+    else
+        return Fibonachi(num - 2) + Fibonachi(num - 1);
+}
+
+
+int main() {
+    setlocale(LC_ALL, "russian");
+    int num;
+    std::cout << "Введите количество чисел: ";
+    std::cin >> num;
+    std::cout << "Числа Фибоначчи: ";
+    for (int i = 1; i <= num; ++i) {
+        std::cout << Fibonachi(i) << "\t";
+
+    }
+    return 0;
+}
+
+
+
+
+// Память константна O(1).
+// Время работы О(2^n)
